97bf8bd0f8291171a57920d60ca38e68db3384cf
Joseph Iles <jgk.iles@gmail.com>
2023-01-24 19:21:25 +0000

    Final changes

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,7 @@
 """Function for constructing model Pipeline.
 """
 
+from typing import Collection
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
@@ -12,23 +13,25 @@ from imblearn.over_sampling import RandomOverSampler
 random_over_sampler = RandomOverSampler()
 
 # Use column transformer to one-hot-encode only the categorical columns
-column_transformer = ColumnTransformer(
-    [
-        (
-            "one_hot_encode",
-            OneHotEncoder(handle_unknown="ignore"),
-            ["marital_status", "workclass"],
-        )
-    ],
-    remainder="passthrough",
-    verbose_feature_names_out=False,
-)
+def column_transformer(encode_columns: Collection = ()):
+    return ColumnTransformer(
+        [
+            (
+                "one_hot_encode",
+                OneHotEncoder(handle_unknown="ignore"),
+                encode_columns,
+            )
+        ],
+        remainder="passthrough",
+        verbose_feature_names_out=False,
+    )
+
 
 # Instantiate RandomForestClassifier
 random_forest_classifier = RandomForestClassifier()
 
 
-def make_pipeline() -> Pipeline:
+def make_pipeline(encode_columns: Collection = ()) -> Pipeline:
     """Returns Pipeline used for Home Insurance Model
 
     Returns:
@@ -37,7 +40,7 @@ def make_pipeline() -> Pipeline:
     pipeline = Pipeline(
         steps=[
             ("random_over_sampler", random_over_sampler),
-            ("encoder", column_transformer),
+            ("encoder", column_transformer(encode_columns)),
             ("random_forest", random_forest_classifier),
         ]
     )
